338d3807aac5dc1436f83509e177ff8a98cdc9b8
offlineProgrammer <mmalaka@gmail.com>
2022-07-22 16:23:02 -0700

    Moving the progress dialog to common

--- lib/features/trip/ui/trip_page/selected_trip_card.dart
+++ lib/features/trip/ui/trip_page/selected_trip_card.dart
@@ -11,7 +11,7 @@ import 'package:my_trip_it/common/navigation/router/routes.dart';
 import 'package:my_trip_it/common/utils/app_constants.dart' as constants;
 import 'package:my_trip_it/models/Trip.dart';
 import 'package:my_trip_it/features/trip/ui/trip_page/delete_trip.dart';
-import 'package:my_trip_it/features/trip/ui/trip_page/upload_progress_dialog.dart';
+import 'package:my_trip_it/common/ui/upload_progress_dialog.dart';
 
 class SelectedTripCard extends ConsumerWidget {
   const SelectedTripCard({Key? key, required this.trip}) : super(key: key);
